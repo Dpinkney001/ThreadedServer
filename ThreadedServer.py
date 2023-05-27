d6c69d8b50da6d6292607c19bb3b5431f40ab75e
Duvall Pinkney <14938639+Dpinkney001@users.noreply.github.com>
2023-05-27 10:52:13 -0400

    Add files via upload

--- ThreadedServer.py
+++ ThreadedServer.py
@@ -1,10 +1,37 @@
 import socket
+import threading
 
 ip_address = '127.0.0.1'
 port_number = 65000
 
+THREADS = []
+CMD_INPUT = []
+CMD_OUTPUT = []
+
+
+def handle_connection(connection,address):
+    msg = connection.recv(1024).decode()
+    while msg !='quit':
+        print (msg)
+        connection.send(msg.encode())
+
+
+def close_connection(connection):
+    connection.close()
+
+
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((ip_address, port_number))
+server_socket.listen(5)
+while True:
+    connection, address = server_socket.accept()
+    t = threading.Thread(target=handle_connection,args=(connection,address))
+    THREADS.append(t)
+    t.start()
+
+
+
 
 server_socket.bind((ip_address, port_number))
 
@@ -14,8 +41,8 @@ client_socket, address = server_socket.accept()
 
 print("Got connection from the client.....{}".format(address))
 msg = client_socket.recv(1024).decode()
+
 while true:
-    
     print (msg.encode())
 
 client_socket.close()
